baff1be25abfc3cf14ff970f86218e310f237013
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-03-23 02:20:41 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Header File for CC in C++
---
+++ cc.h
@@ -0,0 +1,13 @@
+#ifndef CC_H
+#define CC_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void setup(){
+    ios::base::sync_with_stdio(false);
+    cin.tie(false);
+}
+
+#endif
