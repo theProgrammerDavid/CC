3a901b9715049894843bfb9fb276e7095ec23325
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-03-24 00:06:29 +0530

    feat:input output

--- cc.h
+++ cc.h
@@ -2,12 +2,72 @@
 #define CC_H
 
 #include <bits/stdc++.h>
+#include <cstdint>
+#include <cstdlib>
+#include <cstdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <ios>
+#include <limits>
+#include <string.h>
 
 using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
 
-void setup(){
-    ios::base::sync_with_stdio(false);
-    cin.tie(false);
+void setup()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 }
+
+void flushFileBuffer(){
+    cin.ignore(std::numeric_limits<std::streamsize>::max());
+}
+
+void flushInputBuffer()
+{
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void fastInt(int &number)
+{
+
+    bool negative = false;
+    register int c;
+
+    number = 0;
+
+    c = getchar();
+    if (c == '-')
+    {
+    
+        negative = true;
+        c = getchar();
+    }
+    for (; (c > 47 && c < 58); c = getchar())
+        number = number * 10 + c - 48;
+
+    if (negative)
+        number *= -1;
+}
+
+inline int fast_atoi(char *&data)
+{
+    int val = 0;
+
+    do
+    {
+        val = val * 10 + *data++ - '0';
+    } while (*data & (1 << 4)); // '\n' doesn't have that bit
+
+    ++data;
+    return val;
+}
+
+
 
 #endif
