062b7a206cd31846a34ae61d4676c7d7a3a496d1
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-07-06 23:50:09 +0530

    insertion sort

--- prep/practice/insertionSort.cpp
+++ prep/practice/insertionSort.cpp
@@ -4,36 +4,40 @@ using namespace std;
 
 void insertionSort(int *arr, int n)
 {
-    int i = 0,
-        j = 0,
-        key = 0;
-
-    for (i = 1; i < n; i++)
+    int j;
+    for (int i = 1; i < n; i++)
     {
-        key = arr[i];
+        int key = arr[i];
         j = i - 1;
-        
-        char x[] = "lol";
 
-        while (j >= 0 && arr[j] > key)
+        while (j >= 0 and arr[j] > key)
         {
             arr[j + 1] = arr[j];
-            j--;
+            --j;
         }
+
         arr[j + 1] = key;
     }
+}
 
-    for (i = 0; i < n; i++)
+void print(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
     {
-        cout << arr[i] << ' ';
+        std::cout<<arr[i]<<" ";
     }
+    std::cout << '\n';
 }
 
 int main()
 {
     int arr[] = {1, 2, 9, 34, 8, -2, 0, -3, 5, 7, -18};
     int n = 11;
-
+    
+    print(arr,n);
+    
     insertionSort(arr, n);
+    
+    print(arr,n);
     return 0;
 }
